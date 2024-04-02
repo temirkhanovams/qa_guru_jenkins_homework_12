c694ef5b27fb6debae10260a8b56ab5ab73d8958
temirkhanovams <marinchik.t.s@gmail.com>
2024-04-02 23:57:43 +0300

    скрыла настройки webdriver

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,9 @@ def browser_init():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1024
     browser.config.window_height = 780
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
+    # driver_options = webdriver.ChromeOptions()
+    # driver_options.page_load_strategy = 'eager'
+    # browser.config.driver_options = driver_options
 
     yield
 
