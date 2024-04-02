e301974a455ed5b4d596ec8ab04ef8c8337a627b
temirkhanovams <marinchik.t.s@gmail.com>
2024-04-03 00:55:31 +0300

    - Added selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,34 @@
-from selene import browser
 import pytest
-from selenium import webdriver
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+# from utils import attach
 
 @pytest.fixture(scope='function')
-def browser_init():
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1024
-    browser.config.window_height = 780
-    # driver_options = webdriver.ChromeOptions()
-    # driver_options.page_load_strategy = 'eager'
-    # browser.config.driver_options = driver_options
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
-    yield
+    browser = Browser(Config(driver))
+    yield browser
+
+    # attach.add_screenshot(browser)
+    # attach.add_logs(browser)
+    # attach.add_html(browser)
+    # attach.add_video(browser)
 
     browser.quit()
