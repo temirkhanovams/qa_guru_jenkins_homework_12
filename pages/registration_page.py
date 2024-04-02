7d4e89d1a56099649056e31db750810aa5dff245
temirkhanovams <marinchik.t.s@gmail.com>
2024-04-03 02:41:23 +0300

    add allure steps

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -2,6 +2,7 @@ from data.users import User
 from qa_guru_selene_homework_12 import resource
 from selene import browser, be, by, command, have
 
+import allure
 
 class RegistrationPage:
     def __init__(self):
@@ -22,22 +23,28 @@ class RegistrationPage:
         self.submit_selector = browser.element('#submit')
         self.result_selector = browser.element('.table').all('tr td:nth-child(2)')
 
+    @allure.step("Открываем страницу регистрации")
     def open(self):
         return browser.open(f'/automation-practice-form')
 
+    @allure.step("Заполняем Фамилию и имя")
     def fill_full_name(self, first_name, last_name):
         self.first_name_selector.should(be.blank).type(first_name)
         self.last_name_selector.should(be.blank).type(last_name)
 
+    @allure.step("Заполняем email")
     def fill_email(self, value):
         self.email_selector.should(be.blank).type(value)
 
+    @allure.step("Выбираем пол")
     def fill_gender(self, value):
         self.gender_selector.element_by(have.value(value)).element('..').click()
 
+    @allure.step("Вводим номер тел.")
     def fill_phone(self, value):
         self.phone_selector.should(be.blank).type(value)
 
+    @allure.step("Выбираем год, число, месяц рождения")
     def fill_birthday(self, year, month, day):
         browser.element('#dateOfBirthInput').click()
         self.year_selector.click()
@@ -47,25 +54,31 @@ class RegistrationPage:
         self.day_selector.click()
         browser.element(by.text(day)).click()
 
+    @allure.step("Выбираем Subjects")
     def fill_subjects(self, value):
         self.subjects_selector.should(be.blank).type(value).press_enter()
 
+    @allure.step("Выбираем хобби")
     def fill_hobbies(self, param):
         browser.element('#hobbies-checkbox-2').perform(command.js.scroll_into_view)
         browser.all('.custom-checkbox').element_by(have.exact_text(param)).click()
 
+    @allure.step("Загружаем фото")
     def fill_image(self, value):
         self.image_selector.send_keys(resource.path(value))
 
+    @allure.step("Вводим адрес")
     def fill_address(self, value):
         self.address_selector.click().type(value)
 
+    @allure.step("Выбираем штат и город")
     def fill_state(self, state, city):
         self.state_selector.click()
         self.state_city_value_selector.element_by(have.exact_text(state)).click()
         self.city_selector.click()
         self.state_city_value_selector.element_by(have.exact_text(city)).click()
 
+    @allure.step("Отправляем форму")
     def submit(self):
         self.submit_selector.perform(command.js.click)
 
@@ -81,6 +94,7 @@ class RegistrationPage:
         self.fill_address(admin.address)
         self.fill_state(admin.state, admin.city)
 
+    @allure.step("Проверяем инфо о студенте с введённой им значениями")
     def should_registered_user_with(self, admin: User):
         self.result_selector.should(
             have.texts(f'{admin.first_name} {admin.last_name}', {admin.email}, {admin.gender}, {admin.phone},
