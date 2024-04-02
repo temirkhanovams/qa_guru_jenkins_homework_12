f7c73eb67d04b2e7661d7e0e13a318da431be74e
temirkhanovams <marinchik.t.s@gmail.com>
2024-04-03 02:11:53 +0300

    - without Attach

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selene import Browser, Config
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from utils import attach
+# from utils import attach
 
 
 @pytest.fixture(scope='function')
@@ -16,9 +16,9 @@ def setup_browser(request):
     browser = Browser(Config(driver))
     yield browser
 
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
-    attach.add_video(browser)
+    # attach.add_screenshot(browser)
+    # attach.add_logs(browser)
+    # attach.add_html(browser)
+    # attach.add_video(browser)
 
     browser.quit()
