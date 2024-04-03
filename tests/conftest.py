5a26ac00db0a0fe8dbb44361239cb821ba81494d
temirkhanovams <marinchik.t.s@gmail.com>
2024-04-04 02:31:35 +0300

    Добавила папку с тестами по гитхабу

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,32 +1,61 @@
+import os
+
+import allure
 import pytest
 from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ChromeOptions
 
 from utils import attach
+
+DEFAULT_VERSION = '100.0'
+
+@allure.step('Select browser version')
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome', help="Choose browser name.")
+    parser.addoption('--browser_version', default='100.0',
+                     help='Choose browser version. For Chrome: 99.0 or 100.0. For Firefox: 97.0 or 98.0.')
 
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
+    browser_name = request.config.getoption('--browser_name')
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != '' else DEFAULT_VERSION
+    browser_name = 'chrome'
+    driver_options = ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
     browser.config.base_url = "https://demoqa.com"
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
-    options = Options()
     selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserName": browser_name,
+        "browserVersion": browser_version,
         "selenoid:options": {"enableVNC": True, "enableVideo": True}}
-    options.capabilities.update(selenoid_capabilities)
 
-    driver = webdriver.Remote(command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub", options=options)
+    driver_options.capabilities.update(selenoid_capabilities)
+
+    # login = os.getenv('LOGIN')
+    # password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        # command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=driver_options)
 
     browser.config.driver = driver
-    yield browser
 
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
-    attach.add_video(browser)
+    yield browser
+    with allure.step('Add screenshot'):
+        attach.add_screenshot(browser)
+    with allure.step('Add slog'):
+        attach.add_logs(browser)
+    with allure.step('Add HTML'):
+        attach.add_html(browser)
+    with allure.step('Add VIDEO'):
+        attach.add_video(browser)
 
     browser.quit()
