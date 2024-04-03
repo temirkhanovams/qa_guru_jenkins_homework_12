8003e42287a8b9604f79d6f6566bb0ee1c325bc4
temirkhanovams <marinchik.t.s@gmail.com>
2024-04-03 03:55:58 +0300

    Update conftest.py V=122.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,7 @@ def browser_init():
 @pytest.fixture(scope='function')
 def setup_browser(request):
     options = Options()
-    selenoid_capabilities = {"browserName": "chrome", "browserVersion": "100.0",
+    selenoid_capabilities = {"browserName": "chrome", "browserVersion": "122.0",
                              "selenoid:options": {"enableVNC": True, "enableVideo": True}}
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub", options=options)
