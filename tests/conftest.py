d16c4df0265a99a94b9c394ac419d1c5e8cbcec0
temirkhanovams <marinchik.t.s@gmail.com>
2024-04-03 18:28:04 +0300

    Последние штрихи - reformat ctrl+shift+alt+L

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import browser
+
 from utils import attach
 
 
@@ -12,20 +13,11 @@ def setup_browser(request):
     browser.config.window_height = 1080
 
     options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
+    selenoid_capabilities = {"browserName": "chrome", "browserVersion": "100.0",
+        "selenoid:options": {"enableVNC": True, "enableVideo": True}}
     options.capabilities.update(selenoid_capabilities)
 
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
+    driver = webdriver.Remote(command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub", options=options)
 
     browser.config.driver = driver
     yield browser
